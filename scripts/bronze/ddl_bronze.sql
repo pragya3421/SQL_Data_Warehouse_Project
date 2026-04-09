6c847569a69d7e0146184d8b84ad1ba575f0a55d
Pragya Verma <134429754+pragya3421@users.noreply.github.com>
2026-04-09 16:59:50 -0500

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,94 +1,201 @@
-SELECT current_database();
+----Creating SQL DDL Scripts for ALL CSV Files in the CRM & ERP Systems.
 
-CREATE SCHEMA bronze;
-CREATE SCHEMA silver;
-CREATE SCHEMA gold;
+/*
+===========================================
+Bronze Rules
+===========================================
 
------------------------------------------
+- All names must start with the source system name.
+- Table names must match their original names (no renaming).
 
-DROP TABLE IF EXISTS bronze.crm_cust_info;
+Naming Convention:
+    <sourcesystem>_<entity>
 
+    <sourcesystem> : Name of the source system (e.g., crm, erp)
+    <entity>       : Exact table name from the source system
+
+Example:
+    crm_customer_info → Customer information from the CRM system
+
+Notes:
+- Bronze layer stores raw data
+- No transformations should be applied
+- Maintains source system lineage
+
+===========================================
+*/
+
+
+
+--------------------------------------------------
+-- Bronze Tables (RAW LAYER)
+--------------------------------------------------
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info
 (
-cst_id INT, 
-cst_key VARCHAR(50),
-cst_firstname VARCHAR(50),
-cst_lastname VARCHAR(50),
-cst_marital_status VARCHAR(50),
-cst_gndr VARCHAR(50),
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
 cst_create_date DATE
 );
 
-SELECT * FROM bronze.crm_cust_info;
 
---------------------------------------------
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info
+(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
 
-DROP TABLE IF EXISTS bronze.crm_sales_details;
 
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details
 (
-sls_ord_num VARCHAR(50),
-sls_prd_key VARCHAR(50),
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
 sls_cust_id INT,
 sls_order_dt INT,
 sls_ship_dt INT,
 sls_due_dt INT,
-sls_sales INT, 
+sls_sales INT,
 sls_quantity INT,
 sls_price INT
 );
 
-SELECT * FROM bronze.crm_sales_details;
 
------------------------------------------
-
-DROP TABLE IF EXISTS bronze.crm_prd_info;
-
-CREATE TABLE bronze.crm_prd_info
+IF OBJECT_ID ('bronze.erp_LOC_A101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_LOC_A101;
+CREATE TABLE bronze.erp_LOC_A101
 (
-prd_id INT,
-prd_key VARCHAR(50),
-prd_nm VARCHAR(50),
-prd_cost INT,
-prd_line VARCHAR(50),
-prd_start_dt TIMESTAMP,
-prd_end_dt TIMESTAMP
+CID NVARCHAR(50),
+CNTRY NVARCHAR(50)
 );
 
-SELECT * FROM bronze.crm_prd_info;
 
------------------------------------------
 
-DROP TABLE IF EXISTS bronze.erp_cust_az12;
-
-CREATE TABLE bronze.erp_cust_az12 (
-    cid    VARCHAR(50),
-    bdate  DATE,
-    gen    VARCHAR(50)
+IF OBJECT_ID ('bronze.erp_CUST_AZ12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_CUST_AZ12;
+CREATE TABLE bronze.erp_CUST_AZ12
+(
+CID NVARCHAR(50),
+BDATE DATE,
+GEN NVARCHAR(50)
 );
 
-SELECT * FROM bronze.erp_cust_az12;
 
------------------------------------------
 
-DROP TABLE IF EXISTS bronze.erp_loc_a101;
-
-CREATE TABLE bronze.erp_loc_a101 (
-    cid    VARCHAR(50),
-    cntry  VARCHAR(50)
-);
-   
-SELECT * FROM bronze.erp_loc_a101;
-
-------------------------------------------
-
-DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
-
-CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id    		VARCHAR(50),
-    cat 		VARCHAR(50),
-    subcat	    VARCHAR(50),
-	maintenance VARCHAR(50)
+IF OBJECT_ID ('bronze.erp_PX_CAT_G1V2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_PX_CAT_G1V2;
+CREATE TABLE bronze.erp_PX_CAT_G1V2
+(
+ID NVARCHAR(50),
+CAT NVARCHAR(50),
+SUBCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(50)
 );
 
-SELECT * FROM bronze.erp_px_cat_g1v2;
+GO
+
+
+/*
+========================
+BULK INSERT - CRM TABLE
+========================
+*/
+
+--
+TRUNCATE TABLE  bronze.crm_cust_info
+BULK INSERT bronze.crm_cust_info
+FROM '/var/opt/mssql/data/source_crm/cust_info.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+--
+
+TRUNCATE TABLE  bronze.crm_prd_info
+BULK INSERT bronze.crm_prd_info
+FROM '/var/opt/mssql/data/source_crm/prd_info.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+--
+
+TRUNCATE TABLE  bronze.crm_sales_details
+BULK INSERT bronze.crm_sales_details
+FROM '/var/opt/mssql/data/source_crm/sales_details.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+
+/*
+========================
+BULK INSERT - ERP TABLE
+========================
+*/
+
+--
+
+TRUNCATE TABLE  bronze.erp_CUST_AZ12
+BULK INSERT bronze.erp_CUST_AZ12
+FROM '/var/opt/mssql/data/source_erp/CUST_AZ12.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+--
+
+TRUNCATE TABLE  bronze.erp_LOC_A101
+BULK INSERT bronze.erp_LOC_A101
+FROM '/var/opt/mssql/data/source_erp/LOC_A101.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+
+
+--
+
+TRUNCATE TABLE  bronze.erp_PX_CAT_G1V2
+BULK INSERT bronze.erp_PX_CAT_G1V2
+FROM '/var/opt/mssql/data/source_erp/PX_CAT_G1V2.csv'
+WITH 
+(
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
